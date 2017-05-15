d6176cb73bf0ae6b9965d73afe94962d44a41074
Robin Hall <karaken@gmail.com>
2017-05-15 09:39:38 -0400

    Changed license, changed key tables, changed comments to match .NET style with summaries.

---
+++ PSOCrypt-BB/PSOCryptException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSOCrypt.Exceptions
+{
+    [Serializable]
+    public class PSOCryptException : Exception
+    {
+        public PSOCryptException()
+        {
+        }
+
+        public PSOCryptException(string message) : base(message)
+        {
+        }
+
+        public PSOCryptException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
---
+++ PSOCrypt-BB/Tables/IBBKeytable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSOCrypt.Tables
+{
+    public interface IBBKeytable
+    {
+        uint[] Table { get; }
+    }
+}
